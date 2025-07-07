b9188117b40d23eaa541cc47a1b03ca70d783113
ValerieAttila09 <akunvalerie76@gmail.com>
2025-07-07 11:28:03 +0700

    committed

---
+++ api/hello.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express()
+
+app.get('/api/hello', (req, req) => {
+  res.json({
+    message: "Hello from Express on Vercel!"
+  });
+});
+
+export default app;
