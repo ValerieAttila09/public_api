c307fe0888942f8cf0da0435fc0a5459794a0afb
ValerieAttila09 <akunvalerie76@gmail.com>
2025-07-07 12:34:51 +0700

    committed

--- express-app.js
+++ express-app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send("hello from root!");
+});
+
 app.get('/api/hello', (req, res) => {
   res.json({
     message: "Hello from Express on Vercel!"
